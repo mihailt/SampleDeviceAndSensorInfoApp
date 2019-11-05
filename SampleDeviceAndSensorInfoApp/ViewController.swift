8b53e86856ab378c508740cad5ce01e27c2f0871
Mihails Tumkins <mihailt@gmail.com>
2019-11-05 12:48:46 +0200

    Initial Commit

---
+++ SampleDeviceAndSensorInfoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleDeviceAndSensorInfoApp
+//
+//  Created by Mihais Tumkins on 05/11/2019.
+//  Copyright © 2019 Mihails Tumkins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
